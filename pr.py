563963ed3c84e85740aea222486f036aae430577
Martynov Max <m.martynov2007@gmail.com>
2023-05-10 19:22:33 +0400

    areahthstbr

--- pr.py
+++ pr.py
@@ -4,57 +4,42 @@ import json
 
 def draw():
     fnt = ('Helvetica 15 bold')
+    canvas.delete("all")
 
-    for i in range(5):
-        for j in range(5):
+    for i in range(n):
+        for j in range(m):
             color1 = colors[board[i][j][1]]
             color2 = colors[board[i][j][2]]
             ar1 = board[i][j][3]
             ar2 = board[i][j][4]
             shape = board[i][j][0]
-            # if shape == 0:
-            #     canvas.create_rectangle(j * 80, i * 80, (j + 1) * 80, (i + 1) * 80, fill=color1)
-            # if shape == 1:
-            #     canvas.create_polygon(j * 80, i * 80, (j + 1) * 80, (i + 1) * 80, (j + 1) * 80, i * 80, fill=color1,
-            #                           outline='black')
-            # if shape == 2:
-            #     canvas.create_polygon(j * 80, (i + 1) * 80, (j + 1) * 80, (i + 1) * 80, j * 80, i * 80, fill=color1,
-            #                           outline='black')
-            # if shape == 3:
-            #     canvas.create_polygon(j * 80, (i + 1) * 80, (j + 1) * 80, i * 80, j * 80, i * 80, fill=color1,
-            #                           outline='black')
-            #
-            # if shape == 4:
-            #     canvas.create_polygon((j + 1) * 80, i * 80, (j + 1) * 80, (i + 1) * 80, j * 80, (i + 1) * 80,
-            #                           fill=color1, outline='black')
-
             if shape == 0:
-                canvas.create_polygon(j * 80, i * 80, (j + 1) * 80, (i + 1) * 80, (j + 1) * 80, i * 80, fill=color1,
+                canvas.create_polygon(j * w, i * w, (j + 1) * w, (i + 1) * w, (j + 1) * w, i * w, fill=color1,
                                       outline=color1)
 
-                canvas.create_polygon(j * 80, (i + 1) * 80, (j + 1) * 80, (i + 1) * 80, j * 80, i * 80, fill=color2,
+                canvas.create_polygon(j * w, (i + 1) * w, (j + 1) * w, (i + 1) * w, j * w, i * w, fill=color2,
                                       outline=color2)
-                canvas.create_text(j * 80 + 60, i * 80 + 30, text=str(ar1), fill="black", font=fnt)
-                canvas.create_text(j * 80 + 30, i * 80 + 60, text=str(ar2), fill="black", font=fnt)
+                canvas.create_text(j * w + int(w * 0.75), i * w + int(w * 0.375), text=str(ar1), fill="black", font=fnt)
+                canvas.create_text(j * w + int(w * 0.375), i * w + int(w * 0.75), text=str(ar2), fill="black", font=fnt)
             if shape == 1:
-                canvas.create_polygon(j * 80, (i + 1) * 80, (j + 1) * 80, i * 80, j * 80, i * 80, fill=color1,
+                canvas.create_polygon(j * w, (i + 1) * w, (j + 1) * w, i * w, j * w, i * w, fill=color1,
                                       outline=color1)
-                canvas.create_polygon((j + 1) * 80, i * 80, (j + 1) * 80, (i + 1) * 80, j * 80, (i + 1) * 80,
+                canvas.create_polygon((j + 1) * w, i * w, (j + 1) * w, (i + 1) * w, j * w, (i + 1) * w,
                                       fill=color2, outline=color2)
-                canvas.create_text(j * 80 + 30, i * 80 + 30, text=str(ar1), fill="black", font=fnt)
-                canvas.create_text(j * 80 + 60, i * 80 + 60, text=str(ar2), fill="black", font=fnt)
+                canvas.create_text(j * w + int(w * 0.375), i * w + int(w * 0.375), text=str(ar1), fill="black", font=fnt)
+                canvas.create_text(j * w + int(w * 0.75), i * w + int(w * 0.75), text=str(ar2), fill="black", font=fnt)
             if board[i][j][3] != board[i][j][4]:  # диагональ
                 if shape == 0:
-                    canvas.create_line((j + 1) * 80, (i + 1) * 80, j * 80, i * 80, fill='black', width=3)
+                    canvas.create_line((j + 1) * w, (i + 1) * w, j * w, i * w, fill='black', width=3)
                 if shape == 1:
-                    canvas.create_line(j * 80, (i + 1) * 80, (j + 1) * 80, i * 80, fill='black', width=3)
+                    canvas.create_line(j * w, (i + 1) * w, (j + 1) * w, i * w, fill='black', width=3)
             # левая сторона
             if board[i][j][4 - shape] != board[i][j - 1][3 + board[i][j - 1][0]]:
-                canvas.create_line(j * 80, i * 80, j * 80, (i + 1) * 80, fill='black', width=3)
+                canvas.create_line(j * w, i * w, j * w, (i + 1) * w, fill='black', width=3)
             # верхняя сторона
             if board[i][j][3] != board[i - 1][j][4]:
-                canvas.create_line(j * 80, i * 80, (j + 1) * 80, i * 80, fill='black', width=3)
-
+                canvas.create_line(j * w, i * w, (j + 1) * w, i * w, fill='black', width=3)
+    canvas.create_rectangle(1, 1, n * w + 1, m * w + 1, width=3, outline="black")
 
 # board = [[[1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [0, 0, 0, 0, 0], [0, 4, 4, 4, 4], [0, 4, 4, 4, 4], [0, -1, -1, -1, -1]],
 #          [[0, 0, 0, 0, 0], [0, 1, 0, 1, 0], [0, 0, 0, 0, 0], [1, 4, 0, 4, 0], [0, 0, 0, 0, 0], [0, -1, -1, -1, -1]],
@@ -64,15 +49,7 @@ def draw():
 #          [[0, -1, -1, -1, -1], [0, -1, -1, -1, -1], [0, -1, -1, -1, -1], [0, -1, -1, -1, -1], [0, -1, -1, -1, -1],
 #           [0, -1, -1, -1, -1]]]
 
-board = [[[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, -1, -1, -1, -1]],
-         [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, -1, -1, -1, -1]],
-         [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, -1, -1, -1, -1]],
-         [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, -1, -1, -1, -1]],
-         [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, -1, -1, -1, -1]],
-         [[0, 0, 0, 0, 0], [1, 0, 2, 0, 2], [0, 0, 0, 0, 0], [0, 0, 3, 0, 3], [0, 0, 0, 0, 0], [0, -1, -1, -1, -1]],
-         [[0, 2, 2, 2, 2], [0, 2, 2, 2, 2], [0, 0, 0, 0, 0], [1, 3, 3, 3, 5], [0, 3, 3, 5, 5], [0, -1, -1, -1, -1]],
-         [[0, -1, -1, -1, -1], [0, -1, -1, -1, -1], [0, -1, -1, -1, -1], [0, -1, -1, -1, -1], [0, -1, -1, -1, -1],
-          [0, -1, -1, -1, -1]]]
+
 
 
 # board = []
@@ -112,10 +89,10 @@ def key_press(event):
 
 
 def click(event):
-    row = event.y // 80
-    col = event.x // 80
-    x = event.x % 80
-    y = event.y % 80
+    row = event.y // w
+    col = event.x // w
+    x = event.x % w
+    y = event.y % w
     if p < 0:
         board[row][col][0] = (board[row][col][0] + 1) % 2
         draw()
@@ -126,7 +103,7 @@ def click(event):
         else:
             t = 2
     else:
-        if x + y < 80:
+        if x + y < w:
             t = 1
         else:
             t = 2
@@ -135,6 +112,16 @@ def click(event):
     print(colors[board[row][col][t]], board[row][col][t + 2])
     draw()
 
+
+n = m = 15
+w = 45
+board = []
+for i in range(n):
+    line = []
+    for i in range(m):
+        line.append([0, 0, 0, 0, 0])
+    line.append([0, -1, -1, -1, -1])
+    board.append(line)
 
 colors = ["white", "yellow", "green", "blue", "red"]
 p = 0
@@ -155,9 +142,9 @@ mainmenu.add_cascade(label="Файл",
 # helpmenu.add_command(label="О программе")
 # mainmenu.add_cascade(label="Справка",
 #                      menu=helpmenu)
-root.geometry("500x500")
+root.geometry(f"{n * w + 10}x{m * w + 10}")
 
-canvas = Canvas(bg="white", width=400, height=400)
+canvas = Canvas(bg="white", width=n * w, height=m * w)
 canvas.pack()
 draw()
 canvas.bind("<Button>", click)
